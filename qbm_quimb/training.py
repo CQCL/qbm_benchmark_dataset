a177931ec00917907a7015783e57ab73f9eb9fbb
Yuta Kikuchi <yuta.kikuchi@cambridgequantum.com>
2023-09-27 11:46:46 +0900

    define CLI arguments

--- qbm_quimb/training.py
+++ qbm_quimb/training.py
@@ -17,10 +17,10 @@ def qre(eta, h_qbm):
         h_qbm (Any): Hamiltonian of the QBM
     """
     # h = qu.entropy(eta) # do not use because it has log2 inside
-    evals = qu.eigvalsh(eta)
+    evals = qu.eigvalsh(eta).clip(1e-300)
     h = np.sum(evals * np.log(evals))
     # use log base e all the way
-    evals = qu.eigvalsh(h_qbm)
+    evals = qu.eigvalsh(h_qbm).clip(1e-300)
     z = np.sum(np.exp(evals))
     eta_stat = qu.expec(eta, h_qbm)
     return h-eta_stat+qu.log(z)
@@ -69,7 +69,7 @@ def training_qbm(
         target_ham_expectations: list[float],
         target_eta: qu.qarray,
         initial_params: np.ndarray = None, 
-        gamma: float = 0.2,
+        learning_rate: float = 0.2,
         epochs: int = 200, 
         eps: float = 1e-6
     ) -> tuple[np.ndarray, list[np.ndarray], list[float]]:
@@ -80,7 +80,7 @@ def training_qbm(
         target_ham_expectations (list[float]): A list of target hamiltonian expectation values
         target_eta (qu.qarray): Target density matrix
         params (np.ndarray): Parameters of QBM density matrix 
-        gamma (float): Learning rate
+        learning_rate (float): Learning rate
         epochs (int): The number of epochs in the training
         eps (float): Stop traninig when gradient gets smaller than eps
 
@@ -92,24 +92,20 @@ def training_qbm(
     max_grad_hist = []
     qre_hist = []
     
-    if initial_params == None:
-        params = np.zeros(len(model_ham_ops))
-    else:
-        params = initial_params
-
+    params = initial_params
     for _ in range(epochs):
         # create qbm hamiltonian
         model_ham = hamiltonians.total_hamiltonian(model_ham_ops, params)
         model_ham = model_ham.real
 
         # create qbm state
-        qbm_rho = qu.thermal_state(model_ham, 1.0)
+        qbm_rho = qu.thermal_state(model_ham, -1.0)
         qbm_expectations = compute_expectations(qbm_rho, model_ham_ops)
-        qre_hist.append(qre(target_eta, qbm_rho))
+        qre_hist.append(qre(target_eta, model_ham))
 
         # grad and update
         grads = compute_grads(qbm_expectations, target_ham_expectations)
-        params = params - gamma * grads
+        params = params - learning_rate * grads
         max_grad_hist.append(np.max(np.abs(grads)))
         if max_grad_hist[-1] < eps:
             break
