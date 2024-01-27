6af13d25ffdd365b510d19eccf7bd3126a9f511b
Yuta Kikuchi <yuta.kikuchi@cambridgequantum.com>
2024-01-27 09:35:41 +0900

    1d HUB q=4 6 8 pretrain

--- script/run_benchmark.sh
+++ script/run_benchmark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export OMP_NUM_THREADS=64
-target=0
-out="data/tfim"
-for n in 4 6 8 10
+target=4
+out="data/1d_hubbard"
+for n in 4 6 8
 do
 	for beta in 0.5 1 4 10 100
 	do
@@ -10,11 +10,11 @@ do
 		do
 			for eps in 0 0.001 0.01 0.1
 			do
-				for model in 0 1 6 8
+				for model in 8
 				do
-					poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --dn $eps --sn $sigma --output $out
+					poetry run python benchmark.py --qre --t $target --b $beta --n $n --l $model --dn $eps --sn $sigma --output $out
 					if [ $model -eq 8 ]; then
-						poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --dn $eps --sn $sigma --pre_l 6 --output $out
+						poetry run python benchmark.py --qre --t $target --b $beta --n $n --l $model --dn $eps --sn $sigma --pre_l 6 --output $out
 					fi
 				done
 			done
