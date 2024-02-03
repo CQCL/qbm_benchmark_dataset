7e6ddb3c4c71e758de1aad79c5b82762e63db465
Enrico Rinaldi <enrico.rinaldi@quantinuum.com>
2024-02-03 13:40:28 +0900

    [fix] TFIM benchmark script

--- script/run_benchmark.sh
+++ script/run_benchmark.sh
@@ -6,13 +6,13 @@ for n in 4 6 8 10 12
 do
 	for beta in 0.5 1 2 4
 	do
-		for sn in 0
+		for sn in 0 0.1
 		do
-			for dn in 0
+			for dn in 0 0.001
 			do
 				for model in 0
 				do
-					poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --lr 0.5 --dn $dn --sn $sn --output $out
+					poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --lr 0.25 --dn $dn --sn $sn --output $out
 					# if [ $model -eq 8 ]; then
 					# 	poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --pre_l 6 --output $out
 					# fi
