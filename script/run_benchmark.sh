833dcb6d2663d868c801e1a798f154b9fee79314
Enrico Rinaldi <enrico.rinaldi@quantinuum.com>
2024-02-03 13:38:17 +0900

    [file] remove data folders

--- script/run_benchmark.sh
+++ script/run_benchmark.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 export OMP_NUM_THREADS=64
-target=5
-out="data/2d_hubbard"
-for n in 12
+target=0
+out="data/1d_tfim"
+for n in 4 6 8 10 12
 do
-	for beta in 0.5 1 4
+	for beta in 0.5 1 2 4
 	do
-		for sigma in 0 0.1
+		for sn in 0
 		do
-			for eps in 0 0.01
+			for dn in 0
 			do
-				for model in 8
+				for model in 0
 				do
-					poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --dn $eps --sn $sigma --output $out
+					poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --lr 0.5 --dn $dn --sn $sn --output $out
 					# if [ $model -eq 8 ]; then
-					# 	poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --dn $eps --sn $sigma --pre_l 6 --output $out
+					# 	poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --pre_l 6 --output $out
 					# fi
 				done
 			done
