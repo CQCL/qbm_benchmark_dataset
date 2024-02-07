59c83425ec98d8cc697629ad0334675d606e2779
Enrico Rinaldi <enrico.rinaldi@quantinuum.com>
2024-02-07 21:20:10 +0900

    [file] 2d hubbard n=8 l=5

--- script/run_benchmark.sh
+++ script/run_benchmark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export OMP_NUM_THREADS=8
-target=3
-out="data/j1_j2"
-for n in 4 6 8
+target=5
+out="data/2d_hubbard"
+for n in 8
 do
 	for beta in 0.5 1 2 4
 	do
@@ -10,12 +10,12 @@ do
 		do
 			for dn in 0 0.001
 			do
-				for model in 8
+				for model in 5
 				do
 					poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --dn $dn --sn $sn --output $out
-					if [ $model -eq 8 -a $sn -eq 0 -a $dn -eq 0 ]; then
-						poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --pre_l 6 --output $out
-					fi
+					# if [ $model -eq 8 -a $sn -eq 0 -a $dn -eq 0 ]; then
+					# 	poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --pre_l 6 --output $out
+					# fi
 				done
 			done
 		done
