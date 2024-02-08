cefee55953e801494f6a66506403bfb6f5de9c1d
Enrico Rinaldi <enrico.rinaldi@quantinuum.com>
2024-02-08 19:53:20 +0900

    [file] 2D Hubbard n=8 l=9

--- script/run_benchmark.sh
+++ script/run_benchmark.sh
@@ -10,12 +10,12 @@ do
 		do
 			for dn in 0 #0.001
 			do
-				for model in 8
+				for model in 9
 				do
-					#poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --dn $dn --sn $sn --output $out
-					#if [ $model -eq 8 -a $sn -eq 0 -a $dn -eq 0 ]; then
-					poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --pre_l 6 --output $out
-					#fi
+					poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --dn $dn --sn $sn --output $out
+					# if [ $model -eq 8 -a $sn -eq 0 -a $dn -eq 0 ]; then
+					# 	poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --pre_l 6 --output $out
+					# fi
 				done
 			done
 		done
