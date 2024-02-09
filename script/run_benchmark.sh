a2cefb3244487e695e193b8a8529c37a896e959a
Enrico Rinaldi <enrico.rinaldi@quantinuum.com>
2024-02-09 11:07:41 +0900

    [file] TFIM n=4,6,8 l=8,9

--- script/run_benchmark.sh
+++ script/run_benchmark.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 export OMP_NUM_THREADS=8
-target=4
-out="data/1d_hubbard"
+target=0
+out="data/1d_tfim"
 for n in 4 6 8
 do
 	for beta in 0.5 1 2 4
 	do
-		for sn in 0 #0.1
+		for sn in 0 0.1
 		do
-			for dn in 0 #0.001
+			for dn in 0 0.001
 			do
-				for model in 9
+				for model in 8 9
 				do
 					poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --dn $dn --sn $sn --output $out
 					# if [ $model -eq 8 -a $sn -eq 0 -a $dn -eq 0 ]; then
