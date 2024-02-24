f517e34f2610bdffbd3869fda2492d528e9ef76e
Enrico Rinaldi <enrico.rinaldi@quantinuum.com>
2024-02-24 02:42:26 +0000

    [file] New results for report

--- script/run_benchmark.sh
+++ script/run_benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export OMP_NUM_THREADS=8
-target=3
-out="data/j1_j2"
+export OMP_NUM_THREADS=64
+target=1
+out="data/1d_heis"
 for n in 4 6 8
 do
 	for beta in 0.5 1 2 4
@@ -10,9 +10,9 @@ do
 		do
 			for dn in 0 # 0.001
 			do
-				for model in 9
+				for model in 1
 				do
-					poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --dn $dn --sn $sn --output $out
+					poetry run python script/benchmark.py --qre --t $target --lr 0.025 --b $beta --n $n --l $model --dn $dn --sn $sn --output $out
 					# if [ $model -eq 8 -a $sn -eq 0 -a $dn -eq 0 ]; then
 					# 	poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --pre_l 6 --output $out
 					# fi
