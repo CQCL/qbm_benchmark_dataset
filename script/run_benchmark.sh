bbd7dc13269fc28d5d7e57d4e72982af490391c8
Enrico Rinaldi <enrico.rinaldi@quantinuum.com>
2024-02-24 04:58:26 +0000

    [file] TFI with new lr for l=6

--- script/run_benchmark.sh
+++ script/run_benchmark.sh
@@ -6,13 +6,13 @@ for n in 4 6 8
 do
 	for beta in 0.5 1 2 4
 	do
-		for sn in 0 # 0.1
+		for sn in 0 0.1
 		do
-			for dn in 0 # 0.001
+			for dn in 0 0.001
 			do
 				for model in 6
 				do
-					poetry run python script/benchmark.py --qre --t $target --lr 0.025 --b $beta --n $n --l $model --dn $dn --sn $sn --output $out
+					poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --dn $dn --sn $sn --output $out
 					# if [ $model -eq 8 -a $sn -eq 0 -a $dn -eq 0 ]; then
 					# 	poetry run python script/benchmark.py --qre --t $target --b $beta --n $n --l $model --pre_l 6 --output $out
 					# fi
